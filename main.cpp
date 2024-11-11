8da9ea1bc1a9d8dd86d8709830fa1a4f5a0d2439
NootNoot750f <nickholas195@gmail.com>
2024-11-10 23:20:26 -0600

    Commit Message

--- main.cpp
+++ main.cpp
@@ -17,9 +17,20 @@ int main() {
     timer.stop();
 
     int secondsElapsed = timer.elapsed();
-    if(secondsElapsed != -1) {
-        cout << "Time Elapsed: " << secondsElapsed << " seconds" << endl;
-        timer.pretty_print();
+
+    int expectedTime = duration_cast<seconds>(steady_clock::now().time_since_epoch()).count();
+    cout<< "Elapsed Time: " << secondsElapsed << " secpnds" << endl;
+    cout << "Expected Time:" << expectedTime << " seconds" << endl;
+
+    if(secondsElapsed == expectedTime) {
+        cout << "The Test Was Passed:" << endl;
+    }else {
+        cout << "The test failed. Not synced/correct with computer time" << endl;
+
+        if(secondsElapsed != -1) {
+            cout << "Time Elapsed:" << secondsElapsed << " Seconds" << endl;
+            timer.pretty_print();
+        }
 
     }
 
